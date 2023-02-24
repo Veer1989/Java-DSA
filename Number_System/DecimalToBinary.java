d4a7fb0287e8762dfcaf20fca4b91215a0a40c5e
veer <sjaiveer024@gmail.com>
2023-02-24 23:15:06 +0530

    Adding new file

--- Number_System/DecimalToBinary.java
+++ Number_System/DecimalToBinary.java
@@ -1,15 +1,19 @@
+import java.util.Scanner;
+
 public class DecimalToBinary {
     public static void main(String[] args) {
-        int num = 332 ;
-        int bin = 0 ;
-       int pw = 1 ;
-        while(num!=0){
-            int par  = num% 2 ;
-            par*= pw ;
-            num/=2 ;
-             pw*=10 ;
-            bin+=par ;
+        Scanner sc = new Scanner(System.in) ;
+        int dec = sc.nextInt();
+        int par = 0 ;
+        int pw =1 ;
+        int bin = 0;
+        while(dec!=0){
+            par = dec % 2 ;
+            bin += par* pw ;
+            pw*=10 ;
+            dec/=2 ;
         }
         System.out.println(bin);
+
     }
 }
